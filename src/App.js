2815f965124e9c8e222c374fbd51606d5e392fb8
eooomji <dcs0909@naver.com>
2021-01-21 22:18:03 +0900

    2021.01.21.목요일 멘토링

--- src/App.js
+++ src/App.js
@@ -15,17 +15,11 @@ import Hi from "./Hi";
 import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
+import IterationSample from "./lterationSample";
 
 class App extends Component {
   render() {
-    return(
-      <div>
-        <ScrollBox ref = {(ref) => this.scrollBox = ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
