a50dcb64f231e88bf8a8b698af843f497b27cc46
eooomji <dcs0909@naver.com>
2021-01-28 22:03:47 +0900

    2021.01.28.목요일 멘토링

--- src/App.js
+++ src/App.js
@@ -8,18 +8,33 @@ class App extends Component {
   }
 }*/
 
-import React, {Component} from "react";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter";
-import Hi from "./Hi";
-import EventPractice from "./EventPractice";
-import ValidationSample from "./ValidationSample";
-import ScrollBox from "./ScrollBox";
-import IterationSample from "./lterationSample";
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
